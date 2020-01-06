6a18ae6577566a0455ca055e5fe2b079c39328da
johnnylyimo <johnnylyimo@gmail.com>
2020-01-06 20:04:45 +0300

    Added FlutterLogo size property

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -26,7 +26,11 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[FlutterLogo()],
+        children: <Widget>[
+          FlutterLogo(
+            size: 300,
+          )
+        ],
       ),
     );
   }
