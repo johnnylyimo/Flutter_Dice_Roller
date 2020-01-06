d05a8b20f6618464b8c1680d1d53a38fb690abb3
johnnylyimo <johnnylyimo@gmail.com>
2020-01-06 16:10:05 +0300

    Created DicePlayground Statefull widget

--- lib/dice_playground.dart
+++ lib/dice_playground.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class DicePlayground extends StatefulWidget {
+  @override
+  _DicePlaygroundState createState() => _DicePlaygroundState();
+}
+
+class _DicePlaygroundState extends State<DicePlayground> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
