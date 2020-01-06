ecdf72ee1efb8d5eb548f1ad1858d356ce94a7fd
johnnylyimo <johnnylyimo@gmail.com>
2020-01-06 17:02:39 +0300

    Created DicePlayground Statefull widget

--- lib/dice_playground.dart
+++ lib/dice_playground.dart
@@ -15,3 +15,15 @@ class DicePlayground extends StatelessWidget {
     );
   }
 }
+
+class Playground extends StatefulWidget {
+  @override
+  _PlaygroundState createState() => _PlaygroundState();
+}
+
+class _PlaygroundState extends State<Playground> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
