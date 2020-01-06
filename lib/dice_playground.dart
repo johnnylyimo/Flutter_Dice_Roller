0cc238fc23347621239ecc8c6b10b2527c9ad8c1
johnnylyimo <johnnylyimo@gmail.com>
2020-01-06 16:32:36 +0300

    Added appBar title with Text

--- lib/dice_playground.dart
+++ lib/dice_playground.dart
@@ -9,7 +9,9 @@ class _DicePlaygroundState extends State<DicePlayground> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('Dice Roller'),
+      ),
     );
   }
 }
