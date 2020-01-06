0865472ec82aafb677b289748d7ee9bbc825f595
johnnylyimo <johnnylyimo@gmail.com>
2020-01-06 19:52:44 +0300

    Added Navigator to the Future

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -11,7 +11,13 @@ class _SplashScreenState extends State<SplashScreen> {
     // TODO: implement initState
     super.initState();
 
-    Future.delayed(Duration(seconds: 2), () {});
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DicePlayground(),
+          ));
+    });
   }
 
   @override
