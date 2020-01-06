97dc43de3b33d46310d225e2a2b92ab024023057
johnnylyimo <johnnylyimo@gmail.com>
2020-01-06 17:21:45 +0300

    Added RaiseButton color property

--- lib/dice_playground.dart
+++ lib/dice_playground.dart
@@ -38,6 +38,7 @@ class _PlaygroundState extends State<Playground> {
         ),
         RaisedButton(
           child: Text('Roll'),
+          color: Theme.of(context).colorScheme.primary,
         )
       ],
     );
